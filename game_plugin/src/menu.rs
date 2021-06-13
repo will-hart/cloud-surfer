c29b492f5273bbbb1b8c528848ce394805fe8963
William Hart <hart.wl@gmail.com>
2021-06-13 12:40:32 +1000

    Add menu instructions

--- game_plugin/src/menu.rs
+++ game_plugin/src/menu.rs
@@ -6,6 +6,7 @@ pub struct MenuPlugin;
 impl Plugin for MenuPlugin {
     fn build(&self, app: &mut AppBuilder) {
         app.init_resource::<ButtonMaterials>()
+            .add_startup_system(spawn_ui_camera.system())
             .add_system_set(SystemSet::on_enter(GameState::Menu).with_system(setup_menu.system()))
             .add_system_set(
                 SystemSet::on_update(GameState::Menu).with_system(click_play_button.system()),
@@ -29,41 +30,169 @@ impl FromWorld for ButtonMaterials {
     }
 }
 
+struct MenuItem;
+
 struct PlayButton;
+
+fn spawn_ui_camera(mut commands: Commands) {
+    commands.spawn_bundle(UiCameraBundle::default());
+}
 
 fn setup_menu(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     button_materials: Res<ButtonMaterials>,
 ) {
-    commands.spawn_bundle(UiCameraBundle::default());
     commands
-        .spawn_bundle(ButtonBundle {
+        .spawn_bundle(NodeBundle {
             style: Style {
-                size: Size::new(Val::Px(120.0), Val::Px(50.0)),
-                margin: Rect::all(Val::Auto),
-                justify_content: JustifyContent::Center,
+                size: Size::new(Val::Percent(100.), Val::Percent(100.)),
                 align_items: AlignItems::Center,
+                justify_content: JustifyContent::Center,
+                flex_direction: FlexDirection::ColumnReverse,
                 ..Default::default()
             },
             material: button_materials.normal.clone(),
             ..Default::default()
         })
-        .insert(PlayButton)
-        .with_children(|parent| {
-            parent.spawn_bundle(TextBundle {
+        .insert(MenuItem)
+        .with_children(|node| {
+            node.spawn_bundle(TextBundle {
                 text: Text {
                     sections: vec![TextSection {
-                        value: "Play".to_string(),
+                        value: "Cloud Surfer".to_string(),
                         style: TextStyle {
                             font: asset_server.get_handle("fonts/FiraSans-Bold.ttf"),
-                            font_size: 40.0,
+                            font_size: 30.0,
                             color: Color::rgb(0.9, 0.9, 0.9),
                         },
                     }],
                     alignment: Default::default(),
                 },
                 ..Default::default()
+            });
+
+            node.spawn_bundle(TextBundle {
+                style: Style {
+                    margin: Rect {
+                        left: Val::Px(0.),
+                        right: Val::Px(0.),
+                        top: Val::Px(20.),
+                        bottom: Val::Px(0.),
+                    },
+                    ..Default::default()
+                },
+                text: Text {
+                    sections: vec![TextSection {
+                        value: "Use A/D to move the left ship, and J/L to move the right ship."
+                            .to_string(),
+                        style: TextStyle {
+                            font: asset_server.get_handle("fonts/FiraSans-Bold.ttf"),
+                            font_size: 20.0,
+                            color: Color::rgb(0.9, 0.9, 0.9),
+                        },
+                    }],
+                    alignment: Default::default(),
+                },
+                ..Default::default()
+            });
+
+            node.spawn_bundle(TextBundle {
+                text: Text {
+                    sections: vec![TextSection {
+                        value: "Capture coins with your tether, but don't move too far apart"
+                            .to_string(),
+                        style: TextStyle {
+                            font: asset_server.get_handle("fonts/FiraSans-Bold.ttf"),
+                            font_size: 20.0,
+                            color: Color::rgb(0.9, 0.9, 0.9),
+                        },
+                    }],
+                    alignment: Default::default(),
+                },
+                ..Default::default()
+            });
+
+            node.spawn_bundle(TextBundle {
+                style: Style {
+                    margin: Rect {
+                        left: Val::Px(0.),
+                        right: Val::Px(0.),
+                        top: Val::Px(0.),
+                        bottom: Val::Px(20.),
+                    },
+                    ..Default::default()
+                },
+                text: Text {
+                    sections: vec![TextSection {
+                        value: "or the tether will snap!".to_string(),
+                        style: TextStyle {
+                            font: asset_server.get_handle("fonts/FiraSans-Bold.ttf"),
+                            font_size: 20.0,
+                            color: Color::rgb(0.9, 0.9, 0.9),
+                        },
+                    }],
+                    alignment: Default::default(),
+                },
+                ..Default::default()
+            });
+
+            node.spawn_bundle(TextBundle {
+                style: Style {
+                    margin: Rect {
+                        left: Val::Px(0.),
+                        right: Val::Px(0.),
+                        top: Val::Px(20.),
+                        bottom: Val::Px(20.),
+                    },
+                    ..Default::default()
+                },
+                text: Text {
+                    sections: vec![TextSection {
+                        value: "Hit the space bar or play below to start.".to_string(),
+                        style: TextStyle {
+                            font: asset_server.get_handle("fonts/FiraSans-Bold.ttf"),
+                            font_size: 20.0,
+                            color: Color::rgb(0.9, 0.9, 0.9),
+                        },
+                    }],
+                    alignment: Default::default(),
+                },
+                ..Default::default()
+            });
+
+            node.spawn_bundle(ButtonBundle {
+                style: Style {
+                    size: Size::new(Val::Px(120.0), Val::Px(50.0)),
+                    margin: Rect {
+                        left: Val::Px(0.),
+                        right: Val::Px(0.),
+                        top: Val::Px(20.),
+                        bottom: Val::Px(0.),
+                    },
+                    justify_content: JustifyContent::Center,
+                    align_items: AlignItems::Center,
+                    ..Default::default()
+                },
+                material: button_materials.normal.clone(),
+                ..Default::default()
+            })
+            .insert(PlayButton)
+            .with_children(|parent| {
+                parent.spawn_bundle(TextBundle {
+                    text: Text {
+                        sections: vec![TextSection {
+                            value: "Play".to_string(),
+                            style: TextStyle {
+                                font: asset_server.get_handle("fonts/FiraSans-Bold.ttf"),
+                                font_size: 40.0,
+                                color: Color::rgb(0.9, 0.9, 0.9),
+                            },
+                        }],
+                        alignment: Default::default(),
+                    },
+                    ..Default::default()
+                });
             });
         });
 }
@@ -101,7 +230,7 @@ fn click_play_button(
     }
 }
 
-fn despawn_menu(mut commands: Commands, items: Query<Entity, With<PlayButton>>) {
+fn despawn_menu(mut commands: Commands, items: Query<Entity, With<MenuItem>>) {
     for item in items.iter() {
         commands.entity(item).despawn_recursive();
     }
